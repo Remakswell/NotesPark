3af6636cc7eabce329e47fbd428d762dc4898769
Maksym <maksymshymko@gmail.com>
2020-08-02 19:50:38 +0300

    Added utils(editText, validator)

---
+++ app/src/main/java/com/notespark/common/Util/EditTextUtil.kt
@@ -0,0 +1,5 @@
+package com.notespark.common.Util
+
+import android.widget.EditText
+
+fun EditText.trimmedString() = this.text.toString().trim()
---
+++ app/src/main/java/com/notespark/common/Util/ValidatorUtil.kt
@@ -0,0 +1,15 @@
+package com.notespark.common.Util
+
+import java.util.regex.Pattern
+
+object ValidatorUtil {
+    fun isEmailValid(email: String) = Pattern
+        .compile("^[a-zA-Z0-9]+@[a-z]+\\.[a-z]{2,4}\$")
+        .matcher(email)
+        .matches()
+
+    fun isPasswordValid(password: String) = Pattern
+        .compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,30}$")
+        .matcher(password)
+        .matches()
+}
